938ccc904c2b1c48c5dae4f0fa1c866d15481139
Gabriella Agathon <gjstimac@gmail.com>
2021-12-31 17:43:55 -0500

    Added porb command.

--- bot.js
+++ bot.js
@@ -70,6 +70,13 @@ client.on("message", msg => {
         msg.channel.send("https://cdn.discordapp.com/attachments/161297309978591233/903331498294390794/video0_13.mp4");
     }
 
+	if(msg.content.startsWith("!porb")){
+		fs.readdir('./'), (err, files) => {
+			const attachment = new MessageAttachment('./img/porb.png');
+			msg.channel.send(attachment);
+		}
+	}
+
     if(msg.content.startsWith("!honk")){
         fs.readdir('./', (err, files) => {
             const attachment = new MessageAttachment('./img/HONK.jpg');
